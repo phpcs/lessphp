e4f9c14cda7a66e5f8c92be5479fd6574ef839da
bruce <bruce.cheng@carbit.com.cn>
2016-05-18 17:30:57 +0800

    后台可以增加修改日志

--- App/Controller/Admin/ArticleController.php
+++ App/Controller/Admin/ArticleController.php
@@ -7,6 +7,7 @@
 namespace App\Controller\Admin;
 
 use App\Controller\Admin;
+use Illuminate\Support\Facades\DB;
 
 class ArticleController extends FatherController
 {
@@ -29,6 +30,11 @@ class ArticleController extends FatherController
 
     public function edit()
     {
+        $id = $_GET['id'];
+        $sql = "SELECT * FROM article WHERE id=".$id;
+        $res = DB()->query($sql);
+
+        $this->assign('list', $res[0]);
         $this->setTitle('修改日志');
         $this->view();
     }
@@ -36,8 +42,16 @@ class ArticleController extends FatherController
     public function save()
     {
         $data = $this->getInput();
-        if ($data) {
+        if (!$data['id']) {
             $sql = "INSERT INTO article VALUES(NULL, ?, ?, ?, ?, ?)";
+            $insert_data = [$data['title'], $data['content'], $data['cate'], NOW_DATE, NOW_DATE];
+            $res = DB()->insert($sql, $insert_data);
+            if ($res) {
+                echo json_encode(['code'=>'ok']);
+            }
+        } else{
+            $sql = "UPDATE article set title=?,content=?,cate_id=?,create_time=?,update_time=? WHERE id={$data['id']}";
+            var_dump($data);
             $insert_data = [$data['title'], $data['content'], $data['cate'], NOW_DATE, NOW_DATE];
             $res = DB()->insert($sql, $insert_data);
             if ($res) {
